1b859173325c1293bd41412702c0d7cd3ef02e6b
unknown <arvsrini@ARVSRINI-LAP.oradev.oraclecorp.com>
2016-02-15 16:55:41 -0500

    removed old samples

--- old-paas-for-saas-samples/PaaS-SaaS_UIAccelerator/UIAcceleratorExtended/OSCProxyClient/src/com/oracle/ptsdemo/oscproxyclient/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xmlns.oracle.com/apps/sales/opptyMgmt/opportunities/opportunityService/types/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.oracle.ptsdemo.oscproxyclient.types;
