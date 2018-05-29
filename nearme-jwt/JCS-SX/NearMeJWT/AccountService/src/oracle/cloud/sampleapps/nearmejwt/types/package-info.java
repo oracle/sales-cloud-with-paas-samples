af63668e9f810825cde5c87e86a7444ccaeb65e1
Arvind Srinivasamoorthy <arvind.srinivasamoorthy@oracle.com>
2018-05-29 08:12:22 -0400

    Nearme sample removed
    Removed Nearme sample as discussed with Angelo and Amit Gokhru

--- nearme-jwt/JCS-SX/NearMeJWT/AccountService/src/oracle/cloud/sampleapps/nearmejwt/types/package-info.java
+++
@@ -1,3 +0,0 @@
-/* Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xmlns.oracle.com/apps/crmCommon/salesParties/accountService/types/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package oracle.cloud.sampleapps.nearmejwt.types;
